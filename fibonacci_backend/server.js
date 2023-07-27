517936cd0d6b85c949eb4277c952e04316f51c48
Prashasti Karlekar <prashastikarlekar@gmail.com>
2023-07-27 12:06:00 -0500

    Configured CORS middleware to allow requests from frontend.

--- fibonacci_backend/server.js
+++ fibonacci_backend/server.js
@@ -9,9 +9,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/database");
 const fibonacciRoutes = require("./routes/fibonacciRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors()); // Enable CORS for all routes
 
 // Middleware
 app.use(bodyParser.json());
